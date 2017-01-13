d23ba7f9a39ccf72decfc291bedd7096e4d1aa30
test <test@test.com>
2017-01-13 17:19:57 +0800

    init

---
+++ backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'FioB3GAE0s5SHNwLYdaI6K19Pnm-9gNQ',
+        ],
+    ],
+];
